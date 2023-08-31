05698b8ea9b1da9c072b99241ab1efc89364beab
DoVanThien <vicetone125@gmail.com>
2023-08-31 17:27:37 +0700

    Final version

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -9,8 +9,10 @@ import {
 
 import { Text, View } from "../components/Themed";
 import { COLORS, Icons, SIZES } from "../constants";
-import { useEffect, useState } from "react";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState, useContext } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ThemeContext } from "../context/ThemeContextProvider";
+import ToggleButton from "../components/ToggleButton";
 
 export default function ModalScreen(navigation: any) {
   const { theme } = useContext(ThemeContext);
